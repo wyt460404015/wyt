80188c3aec4e880d4f666e682ddf83f7061217a1
wyt <1318495492@qq.com>
2017-03-26 20:39:03 +0800

    完善功能模块

--- untitled1/demo.js
+++ untitled1/demo.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-
    $('.soft li').on('mouseenter',function(){
       $(this).addClass('colorTab').siblings().removeClass('colorTab');
       var current = $(this);
@@ -21,3 +20,31 @@ $(document).ready(function(){
    });
 });
 
+function subCheck(){
+   var city = $('.search input').val();
+   //var url = 'https://free-api.heweather.com/v5/weather?city=';
+   //var apikey = '&key=d50088d4d62649aaa6157d71ea1c94f8';
+   $.ajax({
+      url:'https://free-api.heweather.com/v5/weather?city='+city+'&key=d50088d4d62649aaa6157d71ea1c94f8',
+      type:'GET',
+      success:function(data){
+         //var MyJson = JSON.parse(data);
+         var city = data['HeWeather5'][0]['basic']['city'];
+         var cond = data['HeWeather5'][0]['now']['cond']['txt'];
+         //$('.weather .city').html(city);
+         //$('.weather .cond').html(cond);
+
+          jump(city, cond);
+      },
+      error:function(){
+         alert('出错了');
+      }
+   });
+}
+
+function jump(city, cond){
+
+
+    window.location.href='./demo2.html?city='+city+'&cond='+cond;
+
+}
