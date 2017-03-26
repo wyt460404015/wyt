5b8b223fc680e3d82985f36d6223ece8c09fefd8
wyt <1318495492@qq.com>
2017-03-26 13:35:56 +0800

    删除获取位置的代码

--- untitled1/demo.js
+++ untitled1/demo.js
@@ -19,32 +19,5 @@ $(document).ready(function(){
          }
       });
    });
-   navigator.geolocation.getCurrentPosition(onSuccess,onError);
-   function onSuccess(position){
-      //返回用户位置
-      //经度
-      var longitude =position.coords.longitude;
-      //纬度
-      var latitude = position.coords.latitude;
-   }
-   //失败时
-   function onError(error){
-      switch(error.code){
-         case 1:
-            alert("位置服务被拒绝");
-            break;
-         case 2:
-            alert("暂时获取不到位置信息");
-            break;
-
-         case 3:
-            alert("获取信息超时");
-            break;
-
-         case 4:
-            alert("未知错误");
-            break;
-      }
-   }
 });
 
